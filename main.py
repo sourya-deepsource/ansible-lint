5e15f2d12527345653a73f698adfc22450842984
Sourya Vatsyayan <sourya@deepsource.io>
2021-12-06 17:07:29 +0530

    Add issue descriptions
    Signed-off-by: Sourya Vatsyayan <sourya@deepsource.io>

--- main.py
+++ main.py
@@ -3,12 +3,45 @@ from api.reporter import Pep8CLIOutputProcessor
 from api.runner import CLIRunner
 
 
-class AnsibleCLIRunner(CLIRunner):
-    report_processor = Pep8CLIOutputProcessor
+ISSUE_CODES_MAP = {
+    "git-latest": "E4001",
+    "hg-latest": "E4002",
+    "package-latest": "E4003",
+    "no-relative-paths": "E4004",
 
-    @property
-    def command(self):
-        return ["ansible-lint", "-p", *config.files]
+    "partial-become": "E5001",
+    "unnamed-task": "E5002",
+    "no-handler": "E5003",
+    "deprecated-local-action": "E5004",
+
+    "no-changed-when": "E3001",
+    "deprecated-command-syntax": "E3002",
+    "command-instead-of-module": "E3003",
+    "inline-env-var": "E3004",
+    "command-instead-of-shell": "E3005",
+    "risky-shell-pipe": "E3006",
+}
+
+
+class AnsibleLintOutputParser(Pep8CLIOutputProcessor):
+    def get_issues(self):
+        issues = super().get_issues()
+
+        for issue in issues:
+            # The issue code raised by ansible-lint are different from what DeepSource would raise
+            try:
+                issue_code = ISSUE_CODES_MAP[issue.issue_code]
+            except KeyError:
+                continue
+
+            issue.issue_code = f"ANS-{issue_code}"
+
+        return issues
+
+
+class AnsibleCLIRunner(CLIRunner):
+    report_processor = AnsibleLintOutputParser
+    command = ["ansible-lint", "-p", *config.files]
 
 
 def main():
