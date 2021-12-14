11525edb86504dc01741e1dbb288194a30a49292
Sourya Vatsyayan <sourya@deepsource.io>
2021-12-14 10:03:26 +0530

    Log everything
    Signed-off-by: Sourya Vatsyayan <sourya@deepsource.io>

--- api/reporter.py
+++ api/reporter.py
@@ -14,9 +14,9 @@ class CLIOutputProcessor:
         pass
 
     def process_results(self):
+        print(f"======{self.result.stderr.decode()}======")
+        print(f"======{self.result.stdout.decode()}======")
         if self.result.returncode not in self.ALLOWED_EXIT_CODES:
-            print(f"======{self.result.stderr.decode()}======")
-            print(f"======{self.result.stdout.decode()}======")
             raise OutputProcessingError(f"Invalid exit code {self.result.returncode}")
 
         report = Report()
